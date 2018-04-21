22b1aed00e16ad1ba2214b0d98160326bc994813
Jesse J <morskyjezek@gmail.com>
2018-04-21 11:29:35 -0400

    add python script parse loc json

---
+++ assets/python-scripts/parse-loc-json.py
@@ -0,0 +1,17 @@
+import json
+
+with open('/Users/ricardolpunzalan/Desktop/digcur/assets/loc-101-kittens.json','r') as f:
+    data = json.load(f)
+
+    print(type(data))
+
+count = 0
+for result in data['results']:
+    del result['digitized'], result['mime_type'],result['language'],result['other_title']
+    print(result['description'])
+    count = count + 1
+
+print('Counted',count,'items')
+
+with open('/Users/ricardolpunzalan/Desktop/digcur/assets/loc-101-kittens-readable-results.json','w') as f:
+    json.dump(data['results'], f, indent=2)
